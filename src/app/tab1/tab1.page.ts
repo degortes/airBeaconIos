e228711f8e0d6d9895b9050f12f3c90f7d313324
Danilo <danilodegortes@gmail.com>
2021-05-05 10:20:42 +0200

    log

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,12 @@
 import { Component , OnInit} from '@angular/core';
 import { Plugins } from '@capacitor/core';
 import { IBeaconRegion, IBeaconAdvertisement } from 'capacitor-ibeacon';
+
+function _window(): any { return window; }
  
 const { IBeacon } = Plugins;
+
+
 
 @Component({
   selector: 'app-tab1',
@@ -10,11 +14,17 @@ const { IBeacon } = Plugins;
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+  public uuid: string;
 
-  constructor() {}
+  constructor(public bregion: IBeaconRegion,
+    public beadv: IBeaconAdvertisement) {}
 
   async ngOnInit() {
-    console.log();
+    
+    
+    this.uuid = 'F7826DA6-4FA2-4E98-8024-BC5B71E0893E';
+    console.log(this.beadv);
+    console.log(this.bregion);
     
     await IBeacon.requestAlwaysAuthorization();
     IBeacon.addListener('enteredRegion', (response) => {
@@ -31,6 +41,27 @@ export class Tab1Page implements OnInit {
       minor: 1,
       major: 3
     });
+    let uuid = 'F7826DA6-4FA2-4E98-8024-BC5B71E0893E';
+    this.customLocation(this.uuid, "gigi" )
+  }
+    public customLocation(uuid, didRangeBeaconsInRegionHandler): void {
+
+    console.log('start beacons ' + uuid);
+    let window = _window();
+    let locationManager = window.cordova.plugins.locationManager;
+    console.log(locationManager);
+
+    // Request permission to use location on iOS
+    locationManager.requestAlwaysAuthorization(); 
+
+    locationManager.enableBluetooth();
+
+    let delegate = new locationManager.Delegate();
+    delegate.didRangeBeaconsInRegion = didRangeBeaconsInRegionHandler;
+    locationManager.setDelegate(delegate);
+
+    let beaconRegion = new locationManager.BeaconRegion('ionic-beacon', uuid);
+    locationManager.startRangingBeaconsInRegion(beaconRegion);
   }
 
 }
